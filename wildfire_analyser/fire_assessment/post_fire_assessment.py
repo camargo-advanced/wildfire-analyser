8b91166b98b12bc712265957342e3ac001f7036b
Marcelo <camargo.advanced@gmail.com>
2026-02-07 22:33:00 -0300

    feat: new pypi version

--- wildfire_analyser/fire_assessment/post_fire_assessment.py
+++ wildfire_analyser/fire_assessment/post_fire_assessment.py
@@ -59,8 +59,8 @@ class PostFireAssessment:
         deliverables: List[Deliverable],
         cloud_threshold: int = 100,
         days_before_after: int = 30,
-        pre_fire_mosaic_strategy: str = "best_available_scene_raw",  # legacy strategy
-        post_fire_mosaic_strategy: str = "best_available_scene_raw", # legacy strategy
+        pre_fire_mosaic_strategy: str = "best_available_scene",  
+        post_fire_mosaic_strategy: str = "best_available_scene",
         gcs_bucket: str | None = None,
         verbose: bool = False,
     ):
